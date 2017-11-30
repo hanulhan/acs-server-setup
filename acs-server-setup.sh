eeb23829f83e1aca2e33dcf1d6dae989e774c996
Ubuntu <ubuntu@ip-10-0-2-199.ec2.internal>
2017-11-30 14:28:17 +0000

    save

--- acs-server-setup.sh
+++ acs-server-setup.sh
@@ -2,6 +2,8 @@
 set -x
 echo "colorscheme desert" > /home/ubuntu/.vimrc
 echo "Start apt-get update"
-apt-get update && echo 'y' | sudo apt-get upgrade
+apt-get update 
+
+DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" upgrade
 sleep 2
 echo "Finished apt-get update"

765a144d10e1281024d7aed1a0f41e69cc7f6119
Ubuntu <ubuntu@ip-10-0-2-199.ec2.internal>
2017-11-30 14:16:56 +0000

    save

--- acs-server-setup.sh
+++ acs-server-setup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+set -x
 echo "colorscheme desert" > /home/ubuntu/.vimrc
+echo "Start apt-get update"
+apt-get update && echo 'y' | sudo apt-get upgrade
+sleep 2
+echo "Finished apt-get update"

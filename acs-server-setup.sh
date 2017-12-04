ec8fbf543a1bfe1b76956c0747036384c6ff9a06
Ubuntu <ubuntu@ip-10-0-13-192.ec2.internal>
2017-12-04 11:28:13 +0000

    save workspace

--- acs-server-setup.sh
+++ acs-server-setup.sh
@@ -6,9 +6,10 @@ UPDATE_STATE_FILE=$PATH_TO_FILE/update-state.txt
 
 
 
-
+# Function do set the update state to a file
 function setUpdateState {
    UPDATE_STATE=$1
+   doLog "Set the update State to $1"
    echo $UPDATE_STATE > $UPDATE_STATE_FILE
 }
 
@@ -17,7 +18,11 @@ function doLog {
    echo $1 >> $LOGFILE
 }
 
+function doLogUpdateState {
+    doLog "########## $1 ##########"
+}
 
+#Check if Logfile already exists. 
 if [ ! -f $LOGFILE ];
 then
    touch $LOGFILE
@@ -27,14 +32,15 @@ else
 fi
 
 
+#Check if the update-state-file already exists
 if [ ! -f $UPDATE_STATE_FILE ];
 then
     doLog "UpdateState file does not exists. Create it"
     touch $UPDATE_STATE_FILE
-    setUpdateState 0
+    setUpdateState 1
 else
     UPDATE_STATE=$(< ${UPDATE_STATE_FILE})
-    doLog "File exists. UpdateState= $UPDATE_STATE"
+    doLog "UpdateState= $UPDATE_STATE"
 fi
 
 
@@ -42,10 +48,18 @@ fi
 
 
 case $UPDATE_STATE in
-0)
-   doLog "UpdateState is 0"
-   setUpdateState 1
-   echo "colorscheme desert" > /home/ubuntu/.vimrc
+
+
+1) #Installation step 1. Update packages
+   doLogUpdateState "UPDATE-STATE 1: Update packages list"
+
+
+   # install cronjob to procede execution after restart
+   doLog "==> install cronjob"
+   echo "@reboot /home/ubuntu/acs-server-setup/acs-server-setup.sh" > mycron
+   crontab mycron
+   rm mycron
+
 
    # Disable the release upgrader
    doLog "==> Disabling the release upgrader"
@@ -58,36 +72,78 @@ case $UPDATE_STATE in
       systemctl disable apt-daily.service # disable run when system boot
       systemctl disable apt-daily.timer   # disable timer run
    fi
+   
 
-
-   # Fall through
-   ;&
-
-1)
-   doLog "UpdateState is 1"
-
-
-   echo "@reboot /home/ubuntu/acs-server-setup/acs-server-setup.sh" > mycron
-   crontab mycron
-   rm mycron
-
-
+   # get the ubuntu package list
    apt-get -y update
    sleep 5
-   doLog "==> Performing dist-upgrade (all packages and kernel)"
+   doLog "==> Performing upgrade (all packages and kernel)"
    DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
    sleep 5
 
-   doLog "Finished apt-get upgrade. Reboot now"
+   doLog "==> Performing dist-upgrade (all packages and kernel)"
+   DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade
+   sleep 5
+
+   apt-get -y autoremove
+
+   doLog "==> Finished apt-get upgrade. Reboot now"
    setUpdateState 2
    reboot
    ;;
 
-2)
-   doLog "UPDATE FINISHED"
-   touch $PATH_TO_FILE/UPDATE_FINISHED
+2) # Installation step 2: User specific config
+   doLogUpdateState "UPDATE-STATE 2: User specific config"
+
+   doLog "==> delete crontab for ubuntu"
    crontab -r
+
+   doLog "==> 2.1 Edit vi colorscheme"
+   echo "colorscheme desert" > /home/ubuntu/.vimrc
+
+   doLog "==> 2.1.2 Edit .bashrc"
+   cat $PATH_TO_FILE/bashrc >> /home/ubuntu/.bashrc
+
+   doLog "==> 2.2 change user rights for curl and wget"
+   chmod 744 /usr/bin/curl
+   chmod 744 /usr/bin/wget
+
+   doLog "==> 2.3 install nfs-common"
+   apt-get -y install nfs-common
+
+   doLog "==> 2.4 Allow root only to add cron job"
+   echo "root" > /etc/cron.allow
+   echo "deamon
+         bin
+         smtp
+         deamon
+         nuucp
+         listen
+         nobody
+         noaccess
+         tomcat7
+         ubunt" > /etc/cron.deny
+
+   doLog "==> 2.5 disable ipv6"
+   echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
+   echo "net.ipv6.conf.default.disable_ipv6=1" >> /etc/sysctl.conf
+   echo "net.ipv6.conf.lo.disable_ipv6=2" >> /etc/sysctl.conf
+   sysctl -p
+
+
+   doLog "IPV6 disables " | cat /proc/sys/net/ipv6/conf/all/disable_ipv6
+
+
    setUpdateState 3
+   # Fall through
+   ;&
+
+
+
+3)
+   doLogUpdateState "UPDATE-State 3: UPDATE FINISHED"
+   touch $PATH_TO_FILE/UPDATE_FINISHED
+   setUpdateState 4
    ;;
 
 
---
+++ bashrc
@@ -0,0 +1,15 @@
+if [[ $- == *i* ]] then
+    bind '"\e[A": history-search-backward'
+    bind '"\e[B": history-search-forward'
+fi
+
+INSTANCEID="`sudo curl -s http://169.254.169.254/latest/meta-data/instance-id`"
+SERVER_IDENTIFIER=$INSTANCEID
+EC2_SECURITY_GROUP="`sudo wget -q -O - http://instance-data/latest/meta-data/security-groups || die \"wget security-zone has failed: $?\"`"
+
+if [ "$color_prompt" = yes ]; 
+    then PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@$EC2_SECURITY_GROUP\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@$EC2_SECURITY_GROUP:\w\$ '
+fi
+
